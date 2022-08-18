dfa80d98e92b87e68a20ec83403e7e0858fd0e92
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 10:44:03 +0300

    Add data group templates seed

---
+++ 2. Seed Data/05 Seed Data Group Templates.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.DataGroupTemplates (Name, DataGroupTypeId)
+     VALUES
+           ('Person', 1),
+		   ('Spouse', 1),
+		   ('Contact', 2),
+		   ('Parent', 1),
+		   ('PaymentInformation', 3),
+		   ('DeliveryAddress', 4),
+		   ('Employer', 5),
+		   ('EmployerAddress', 4),
+		   ('WorkExperience', 7),
+		   ('Education', 8)
