138de42c72a3edb66edf65f35d973c6473008750
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 10:07:06 +0300

    Add data group types seed

---
+++ 2. Seed Data/04 Seed Data Group Types.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.DataGroupTypes (Value)
+     VALUES
+           ('Person'),
+		   ('ContactInformation'),
+		   ('PaymentInformation'),
+		   ('Address'),
+		   ('Company'),
+		   ('Location'),
+		   ('WorkExperience'),
+		   ('Education'),
+		   ('Residence'),
+		   ('Vehicle')
