530a9bf764d2b374fe5e1baf1affb57ea32ce3a2
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 09:49:03 +0300

    Add seed data types

---
+++ 2. Seed Data/02 Seed Data Types.sql
@@ -0,0 +1,13 @@
+INSERT INTO dbo.DataTypes (Value)
+     VALUES
+           ('Text'),
+		   ('Email'),
+		   ('Phone'),
+		   ('CNP'),
+		   ('Date'),
+		   ('PostalCode'),
+		   ('CreditCard'),
+		   ('TravelID'),
+		   ('MaritalStatus'),
+		   ('StreetName'),
+		   ('CountryCode')
