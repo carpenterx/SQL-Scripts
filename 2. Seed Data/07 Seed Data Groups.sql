c6cfe03a9e51eabbf21e9a176038260c72bd8804
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 11:40:13 +0300

    Add data groups seed

---
+++ 2. Seed Data/07 Seed Data Groups.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.DataGroups (Name, DataGroupTypeId, UserId)
+     VALUES
+           ('Sorin1', 1, 1),
+		   ('Sorin1 Brother', 1, 1),
+		   ('Sorin1 Sister', 1, 1),
+		   ('Sorin1 Contact', 3, 1),
+		   ('Sorin1 Payment', 5, 1),
+		   ('Sorin1 Address', 6, 1),
+		   ('Sorin3', 1, 3),
+		   ('Sorin3 Contact', 3, 3),
+		   ('Sorin7', 1, 7),
+		   ('Sorin7 Contact', 3, 7)
