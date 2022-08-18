115bf6b20285944a911cbc9341c0e43e6b36e2bf
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 11:24:15 +0300

    Add data group templates data templates seed

---
+++ 2. Seed Data/06 Seed Data Group Templates - Data Templates.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.DataGroupTemplatesDataTemplates (DataGroupTemplateId, DataTemplateId)
+     VALUES
+           (1, 1),
+		   (1, 2),
+		   (1, 5),
+		   (1, 6),
+		   (3, 3),
+		   (3, 4),
+		   (6, 7),
+		   (6, 8),
+		   (6, 9),
+		   (5, 10)
