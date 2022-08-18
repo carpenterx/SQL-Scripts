dc60352f5e0a917f39fd2c11c3c994236971b69d
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-18 09:57:36 +0300

    Add data templates seed

---
+++ 2. Seed Data/03 Seed Data Templates.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.DataTemplates (Placeholder, TypeId)
+     VALUES
+           ('FirstName', 1),
+		   ('LastName', 1),
+		   ('Email', 2),
+		   ('PhoneNumber', 3),
+		   ('CNP', 4),
+		   ('DateOfBirth', 5),
+		   ('CountryCode', 11),
+		   ('PostalCode', 6),
+		   ('StreetName', 10),
+		   ('CreditCardNumber', 7)
